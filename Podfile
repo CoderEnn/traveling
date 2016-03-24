c995b94d2037105c1f17fa3c721bf5dd88c07837
zz <zz@zzdeMacBook-Pro.local>
2016-03-24 18:54:14 +0800

    create project

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'travel' do
+
+end
+
+pod 'AVOSCloud'               # 数据存储、短信、云引擎调用等基础服务模块
+pod 'AVOSCloudIM'             # 实时通信模块
+pod 'AVOSCloudCrashReporting' # 错误报告模块
---
+++ travel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travel
+//
+//  Created by zz on 16/3/24.
+//  Copyright © 2016年 zz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
